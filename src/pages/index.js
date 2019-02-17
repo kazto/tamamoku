ef2252ab0c9b4f82e41342c0ca452bf206bfbfca
Kazto TAKAHASHI <kazto@kazto.net>
2019-02-17 16:07:29 +0900

    fix title page

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,13 +8,11 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>たまもく</h1>
+    <p>
+      <a href="https://twitter.com/tamamokumoku">Twitter</a>
+      <a href="https://tamamoku.connpass.com">Connpass</a>
+    </p>
   </Layout>
 )
 
